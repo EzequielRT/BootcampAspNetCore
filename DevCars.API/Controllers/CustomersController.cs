30dff402d31834271562302afb83204c8f36e6b6
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-03-31 20:34:11 -0300

    Definindo rotas da CustomersController

--- DevCars.API/Controllers/CustomersController.cs
+++ DevCars.API/Controllers/CustomersController.cs
@@ -9,5 +9,25 @@ namespace DevCars.API.Controllers
     [Route("api/customers")]
     public class CustomersController : ControllerBase
     {
+        // POST api/customers
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        // POST api/customers/2/orders
+        [HttpPost("{id}")]
+        public IActionResult PostOrder(int id)
+        {
+            return Ok();
+        }
+
+        // GET api/customers/2/orders
+        [HttpGet("{id}/orders/{orderid}")]
+        public IActionResult GetOrder(int id, int orderid)
+        {
+            return Ok();
+        }
     }
 }
