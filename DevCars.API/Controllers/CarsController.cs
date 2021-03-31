2740afb1bcc1f357dcce58b69f3ebbd18dc2b3bd
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-03-31 20:01:11 -0300

    Definindo rotas base de acesso a API

--- DevCars.API/Controllers/CarsController.cs
+++ DevCars.API/Controllers/CarsController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevCars.API.Controllers
 {
-    public class CarsController
+    [Route("api/cars")]
+    public class CarsController : ControllerBase
     {
     }
 }
